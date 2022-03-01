dc5ce60f3abfd7b3b4c611741d7005f6b73f45ce
TC McCarthy <tc.mccarthy1@gmail.com>
2022-02-28 19:41:06 -0500

    It works!

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 /**
  * SILENCE IS GOLDEN
  */
+
+const body = document.querySelector("body");
+const lightswitches = document.querySelectorAll(".switch");
+
+lightswitches.forEach(function (lightswitch) {
+  lightswitch.addEventListener("click", function (e) {
+    e.preventDefault(); // prevent the default behavior on the click
+    body.classList.toggle("light-off");
+  });
+});
